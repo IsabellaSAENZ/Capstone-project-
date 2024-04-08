8100f19213c3a38672368db543e675ab362d0043
Isabella Saenz <145500885+IsabellaSAENZ@users.noreply.github.com>
2024-04-08 22:24:31 +0000

    additions
    added p tag to products page and margin, and added margin to cards on product page

--- products.js
+++ products.js
@@ -15,7 +15,7 @@ let products = document.getElementById("products");
 
 array.map( (item) =>{
 
-  let string = ` <div class="card my-2" style="width: 18rem;">
+  let string = ` <div class="card my-3" style="width: 18rem;">
   <img src="${item.image}" class="card-img-top" >
   <div class="card-body">
   <h5 class="card-title">${item.name}</h5>
